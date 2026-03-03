b69cf159e070909884dc975a1d12395d595f6e1f
Siala <sialasolutions@gmail.com>
2026-03-03 19:38:34 +0200

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,7 @@ export function Navbar({ currentPage, navigate }: NavbarProps) {
             src="/assets/logo.png"
             alt="Codiscan"
             loading="lazy"
-            className="w-12 h-12 rounded-lg" />
+            className="w-20 h-20 rounded-lg" />
 
         </button>
 
